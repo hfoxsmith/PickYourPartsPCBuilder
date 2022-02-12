014d2e3382ba4f6664dcd1f4d3ac0258fdd70026
john041 <70787077+john041@users.noreply.github.com>
2022-02-12 00:16:24 -0700

    Create Insert Dummy Data for Relationship Tables.sql

---
+++ Scripts/Insert Dummy Data for Relationship Tables.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.hasGPU (gpuID, buildID) VALUES (1, 1);
+
+INSERT INTO public.hasStorage (storageID, buildID) VALUES (1, 1);
+
+INSERT INTO public.hasCooling (coolingID, buildID) VALUES (1, 1);
+
+INSERT INTO public.hasRam (ramID, buildID) VALUES (1, 1);
+
+INSERT INTO public.storageCompatibility (storageID, motherboardID) VALUES (1, 1);
+
+INSERT INTO public.cpuCompatibility (cpuID, motherboardID) VALUES (1, 1);
+
+INSERT INTO public.ramCompatibility (ramID, motherboardID) VALUES (1, 1);
+
+INSERT INTO public.caseCompatibility (caseID, motherboardID) VALUES (1, 1);
+
+INSERT INTO public.PROCESSOR_compatableRAM (compatableRAM, cpuID) VALUES (1, 1);
