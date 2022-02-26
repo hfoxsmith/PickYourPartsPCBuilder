be4ace35bcfd8a21d732c5e9457c30cf87df8bdf
Braden-Muns <83435220+Braden-Muns@users.noreply.github.com>
2022-02-26 15:13:26 -0700

    Update INSERT Users.sql

--- Scripts/INSERT Users.sql
+++ Scripts/INSERT Users.sql
@@ -1,1 +1,1 @@
-INSERT INTO public.USERS (id, isAdmin, phoneNumber, email, password, userName, lastName, firstName) VALUES (1, 0, 777-777-7777, alfred@example.com, alf, guy, alfred);
+INSERT INTO public.USERS (userid, isadmin, phonenumber, email, password, username, lastname, firstname) VALUES (1, 0, 777-777-7777, 'alfred@example.com', 'alf33', 'alf', 'guy', 'alfred');
